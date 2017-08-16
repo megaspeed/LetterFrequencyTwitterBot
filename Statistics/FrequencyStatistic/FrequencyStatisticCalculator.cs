230094af197389e8de0b1800c78745f67c0b2d6a
Kirill <megaspeed@live.ru>
2017-08-16 19:22:53 +0700

    Calculate statistics only by letter symbols regardless of letter's case

--- Statistics/FrequencyStatistic/FrequencyStatisticCalculator.cs
+++ Statistics/FrequencyStatistic/FrequencyStatisticCalculator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Statistics
@@ -7,7 +8,7 @@ namespace Statistics
     {
         public FrequencyStatistic ComputeStatistic(IEnumerable<string> messages)
         {
-            var allChars = string.Join("", messages).ToArray();
+            var allChars = string.Join("", messages).Where(Char.IsLetter).Select(Char.ToLower).ToArray();
             var frequencyStatistic = new FrequencyStatistic();
 
             if (allChars.Length == 0)
